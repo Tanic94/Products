9c271744e27c4edfa1a0d043081645ea3bdfcf0e
Nemanja Tanic <necko.tanic@gmail.com>
2020-05-23 23:03:40 +0200

    List of products and formular for new products

--- Products/Products/Controllers/HomeController.cs
+++ Products/Products/Controllers/HomeController.cs
@@ -26,5 +26,25 @@ namespace Products.Controllers
             IQueryable<Product> products = dbConnecion.Products.OrderBy(p => p.Name);
             return View(products);
         }
+        public ActionResult AddProduct(string nameOfProduct, string descriptionOfProduct, string categoryOfProduct, string manufacturerOfProduct,
+            string supplierOfProduct, int priceOfProduct)
+        {
+            IQueryable<Product> returnProducts = dbConnecion.Products.Where(p => p.Name.Equals(nameOfProduct));
+            if (returnProducts.Count() == 0)
+            {
+
+                Product newProduct = new Product();
+                newProduct.ProductID = System.Guid.NewGuid();
+                newProduct.Name = nameOfProduct;
+                newProduct.Description = descriptionOfProduct;
+                newProduct.Category = categoryOfProduct;
+                newProduct.Manufacturer = manufacturerOfProduct;
+                newProduct.Supplier = supplierOfProduct;
+                newProduct.Price = priceOfProduct;
+                dbConnecion.Products.Add(newProduct);
+            }
+            dbConnecion.SaveChanges();
+            return RedirectToAction("Product");
+        }
     }
 }
